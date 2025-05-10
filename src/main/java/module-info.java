8f0a1bee680602748b9962a5eff65765d32ff717
michlopicki <michal.chlopicki@student.uj.edu.pl>
2025-05-10 14:38:27 +0200

    NOT intuitive playing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module tcs.bridge {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens tcs.bridge.controller to javafx.fxml;
--- src/main/java/tcs/bridge/view/ViewApplication.java
+++ src/main/java/tcs/bridge/view/ViewApplication.java
@@ -3,26 +3,34 @@ package tcs.bridge.view;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import tcs.bridge.model.*;
 
+
 import java.util.List;
 
 public class ViewApplication extends Application {
+    static Game game = new Game();
+    static Player playerNorth = new Player(Player.Position.NORTH);
+    static Player playerEast = new Player(Player.Position.EAST);
+    static Player playerSouth = new Player(Player.Position.SOUTH);
+    static Player playerWest = new Player(Player.Position.WEST);
 
-    public Scene makeTable(List<Hand> deal){
-        // Setting a scene with
+
+    public Scene makeTable(List<Hand> deal, Stage stage){
+        // Setting a scene
         BorderPane deck = new BorderPane();
-
+        StackPane table = new StackPane();
+        table.setAlignment(Pos.CENTER);
+        // adding all cards to deck
         for (int i = 0; i < deal.size(); i++) {
-            StackPane north = new StackPane();
-            north.setAlignment(Pos.CENTER);
+            StackPane player = new StackPane();
+            player.setAlignment(Pos.CENTER);
             int translate = -283;
-            for (Card card : deal.get(0).getCards()) {
+            for (Card card : deal.get(i).getCards()) {
                 ImageView imageView = new ImageView(String.valueOf(ViewApplication.class.
                         getResource("/tcs/bridge/view/cards/" +
                                 card.getSuit().getName().toLowerCase() + "_" + card.getRank().getName() + ".png")));
@@ -32,36 +40,64 @@ public class ViewApplication extends Application {
                     imageView.setTranslateX(translate += 40);
                 else
                     imageView.setTranslateY(translate += 40);
-                imageView.getOnMouseClicked();
-                north.getChildren().add(imageView);
+                int finalI = i;
+                // if player clicked good card, play it and move it on the table
+                imageView.setOnMouseClicked(event -> {
+                    if (game.playCard(card)){
+                        player.getChildren().remove(imageView);
+                        imageView.setTranslateX(0); imageView.setTranslateY(0);
+                        switch (finalI) {
+                            case 0: imageView.setTranslateY(-50); break;
+                            case 1: imageView.setTranslateX(50); break;
+                            case 2: imageView.setTranslateY(50); break;
+                            case 3: imageView.setTranslateX(-50); break;
+                        }
+                        table.getChildren().add(imageView);
+                    }
+                });
+                player.getChildren().add(imageView);
             }
             switch (i){
-                case 0: deck.setTop(north); break;
-                case 1: deck.setRight(north); break;
-                case 2: deck.setBottom(north); break;
-                case 3: deck.setLeft(north); break;
+                case 0: deck.setTop(player); break;
+                case 1: deck.setRight(player); break;
+                case 2: deck.setBottom(player); break;
+                case 3: deck.setLeft(player); break;
             }
         }
 
-        deck.setCenter(new Button("Table"));
+        deck.setCenter(table);
         deck.setStyle("-fx-background-color: #32442d;");
 
         //Creating a scene object
         return new Scene(deck, 900, 900);
     }
 
+
+    /* MAIN LOOP OF THE GAME */
     @Override
     public void start(Stage stage) throws Exception {
         //Instantiating the BorderPane class
         Deck deck = new Deck();
-
-        Scene scene = makeTable(deck.shuffle().deal());
-
+        Scene scene = makeTable(game.getDeck().deal(), stage);
         stage.setTitle("Deck View");
         stage.setScene(scene);
         stage.show();
     }
 
     // testowe
-    public static void main(String[] args) {launch();}
+    public static void main(String[] args) {
+        /* MAKING SIMPLE GAME */
+        game.joinGame(playerNorth);
+        game.joinGame(playerEast);
+        game.joinGame(playerSouth);
+        game.joinGame(playerWest);
+
+        Bidding.Bid bid = new Bidding.Bid(1, Suit.HEARTS);
+        Bidding.Bid passBid = Bidding.Bid.PASS_BID;
+        game.makeBid(bid);
+        game.makeBid(passBid);
+        game.makeBid(passBid);
+        game.makeBid(passBid);
+        launch();
+    }
 }
