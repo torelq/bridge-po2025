2bc37c65867e49801495a0a6e6513d3fa84eaa60
torelq <torelq@gmail.com>
2025-05-26 17:16:54 +0200

    Message streams hopefully complete.

---
+++ src/main/java/tcs/bridge/communication/sockets/TCPMessageStream.java
@@ -0,0 +1,37 @@
+package tcs.bridge.communication.sockets;
+
+import tcs.bridge.communication.messages.Message;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPMessageStream implements MessageStream {
+    private final Socket socket;
+    private final ObjectInputStream objectInputStream;
+    private final ObjectOutputStream objectOutputStream;
+
+    public TCPMessageStream(InetAddress address, int port) throws IOException {
+        socket = new Socket(address, port);
+        objectInputStream = new ObjectInputStream(socket.getInputStream());
+        objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+    }
+
+    public Message readMessage() throws IOException {
+        try {
+            return (Message) objectInputStream.readObject();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void writeMessage(Message message) throws IOException {
+        objectOutputStream.writeObject(message);
+    }
+
+    public void close() throws IOException {
+        socket.close();
+    }
+}
