e3063a4617638aed10ece45b1d11252a4ebd0b78
torelq <torelq@gmail.com>
2025-05-26 12:13:10 +0200

    added StringMessage

--- src/main/java/tcs/bridge/communication/Message.java
+++ src/main/java/tcs/bridge/communication/Message.java
@@ -1,6 +1,15 @@
 package tcs.bridge.communication;
 import java.io.Serializable;
 
-public abstract class Message implements Serializable {
-    //
+public interface Message extends Serializable {
+    /*
+        Each message shall be a record with maximal non-null protection.
+        All implementing classes shall be documented here:
+
+        Client to Server:
+        - MakeBidMessage
+
+        Others:
+        - StringMessage (for test/debug purposes)
+    */
 }
