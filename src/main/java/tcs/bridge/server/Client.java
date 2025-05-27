2298eb353ed168ea41eda3a4b7d288b85e4929cf
torelq <torelq@gmail.com>
2025-05-27 16:41:50 +0200

    More server WIP

--- src/main/java/tcs/bridge/server/Client.java
+++ src/main/java/tcs/bridge/server/Client.java
@@ -1,12 +1,97 @@
 package tcs.bridge.server;
+import tcs.bridge.communication.messages.ClientToServerMessage;
+import tcs.bridge.communication.messages.ServerToClientMessage;
+import tcs.bridge.communication.streams.ServerMessageStream;
 import tcs.bridge.model.Player;
 
+import java.io.IOException;
+import java.util.concurrent.*;
+
 class Client {
+
+    class ClientHandler {
+        private boolean defunct=false, disconnectEventPut=false;
+        final ServerMessageStream messageStream;
+        private final BlockingQueue<ServerToClientMessage> outputQueue = new LinkedBlockingQueue<>(50);
+        final BlockingQueue<Event> eventQueue;
+        private final Future<?> readerFuture, writerFuture;
+        private final CountDownLatch countDownLatch = new CountDownLatch(2);
+
+        ClientHandler(ServerMessageStream messageStream, BlockingQueue<Event> eventQueue, ExecutorService executorService) {
+            this.messageStream = messageStream;
+            this.eventQueue = eventQueue;
+            readerFuture = executorService.submit(this::readerThread);
+            writerFuture = executorService.submit(this::writerThread);
+        }
+
+        private void readerThread() {
+            try {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        ClientToServerMessage message = messageStream.readMessage();
+                        eventQueue.put(new MessageEvent(Client.this, message));
+                    }
+                } catch (IOException | InterruptedException ignored) {}
+
+                putDisconnectEvent();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+
+        private void writerThread() {
+            try {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        ServerToClientMessage message = outputQueue.take();
+                        messageStream.writeMessage(message);
+                    }
+                } catch (IOException | InterruptedException ignored) {}
+
+                putDisconnectEvent();
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+
+        synchronized void putDisconnectEvent() {
+            while ((!defunct) && (!disconnectEventPut)) {
+                try {
+                    eventQueue.put(new DisconnectEvent(Client.this));
+                    disconnectEventPut = true;
+                    return;
+                } catch (InterruptedException ignored) {}
+            }
+        }
+
+        synchronized void interrupt() {
+            if (defunct) return;
+            defunct = true;
+            readerFuture.cancel(true);
+            writerFuture.cancel(true);
+            while (true) {
+                try {
+                    countDownLatch.await();
+                    return;
+                } catch (InterruptedException ignored) {}
+            }
+        }
+
+        synchronized void writeMessage(ServerToClientMessage message) {
+            if (defunct) return;
+            try {
+                outputQueue.add(message);
+            } catch (IllegalStateException e) {
+                putDisconnectEvent();
+            }
+        }
+    }
+
 
     final ClientHandler clientHandler;
     Player.Position position=null;
 
-    Client (ClientHandler clientHandler) {
-        this.clientHandler = clientHandler;
+    Client (ServerMessageStream messageStream, BlockingQueue<Event> eventQueue, ExecutorService executorService) {
+        this.clientHandler = new ClientHandler(messageStream, eventQueue, executorService);
     }
 }
--- src/main/java/tcs/bridge/server/Server.java
+++ src/main/java/tcs/bridge/server/Server.java
@@ -1,20 +1,29 @@
 package tcs.bridge.server;
 
+import tcs.bridge.communication.messages.ClientToServerMessage;
+import tcs.bridge.communication.messages.StringMessage;
+import tcs.bridge.communication.streams.ClientMessageStream;
+import tcs.bridge.communication.streams.ServerMessageStream;
+import tcs.bridge.communication.streams.TCPMessageStream;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
-
     private Thread acceptorTh, mainLoopTh;
     BlockingQueue<Event> eventQueue;
     private int port;
     private final ExecutorService clientHandlerExecutor = Executors.newCachedThreadPool();
+
+    private final MainLoop mainLoop = new MainLoop();
 
     private void acceptorThread(ServerSocket serverSocket) {
         try {
@@ -37,18 +46,106 @@ public class Server {
         }
     }
 
-    private void mainLoopThread() {
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                Event event = eventQueue.take();
-            } catch (InterruptedException ignored) {}
+    private class MainLoop {
+        private final Set<Client> clients = new HashSet<>();
+
+        private void mainLoopThread() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    Event event = eventQueue.take();
+
+                    if (event instanceof ConnectEvent connectEvent) {
+                        handleConnectEvent(connectEvent);
+                    } else if (event instanceof DisconnectEvent disconnectEvent) {
+                        handleDisconnectEvent(disconnectEvent);
+                    } else if (event instanceof MessageEvent messageEvent) {
+                        handleMessageEvent(messageEvent);
+                    }
+
+                } catch (InterruptedException ignored) {}
+            }
+        }
+
+        private void handleConnectEvent(ConnectEvent connectEvent) {
+            if (connectEvent instanceof LocalConnectEvent localConnectEvent) {
+                clients.add(new Client(localConnectEvent.serverMessageStream(), eventQueue, clientHandlerExecutor));
+            } else if (connectEvent instanceof TCPConnectEvent tcpConnectEvent) {
+                try {
+                    clients.add(new Client(new ServerMessageStream(new TCPMessageStream(tcpConnectEvent.socket())), eventQueue, clientHandlerExecutor));
+                } catch (IOException e) {
+                    try {
+                        tcpConnectEvent.socket().close();
+                    } catch (IOException e2) {
+                        e.printStackTrace();
+                        e2.printStackTrace();
+                    }
+                }
+
+            } else {
+                throw new RuntimeException();
+            }
+        }
+
+        private void handleDisconnectEvent(DisconnectEvent disconnectEvent) {
+            if (clients.contains(disconnectEvent.client())) {
+                clients.remove(disconnectEvent.client());
+                // TODO: Unregister the client
+                disconnectEvent.client().clientHandler.interrupt();
+            }
+        }
+
+        private void handleMessageEvent(MessageEvent messageEvent) {
+            Client client = messageEvent.from();
+            ClientToServerMessage message = messageEvent.message();
+            if (!clients.contains(client)) return;
+
+            if (message instanceof StringMessage stringMessage) {
+                handleStringMessage(client, stringMessage);
+            } else {
+                throw new RuntimeException();
+            }
+        }
+
+        private void handleShutdownEvent() {
+            acceptorTh.interrupt();
+
+            for (Client client : clients) {
+                client.clientHandler.interrupt();
+            }
+
+            clientHandlerExecutor.shutdownNow();
+
+            while (true) {
+                try {
+                    acceptorTh.join();
+                    break;
+                } catch (InterruptedException ignored) {}
+            }
+
+            while (!eventQueue.isEmpty()) {
+                try {
+                    Event event = eventQueue.take();
+                    if (event instanceof TCPConnectEvent tcpConnectEvent) {
+                        try {
+                            tcpConnectEvent.socket().close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                } catch (InterruptedException ignored) {}
+            }
+
+            Thread.currentThread().interrupt();
+        }
+
+        private void handleStringMessage(Client client, StringMessage message) {
+            System.out.println("SERVER: StringMessage from "+client.position+": \""+message+"\"");
         }
     }
 
     // Blocking
     private void run() {
-        try {
-            ServerSocket socket = new ServerSocket(0);
+        try (ServerSocket socket = new ServerSocket(0)) {
             port = socket.getLocalPort();
             acceptorTh = new Thread(() -> acceptorThread(socket));
         } catch (IOException e) {
@@ -56,7 +153,7 @@ public class Server {
             return;
         }
 
-        mainLoopTh = new Thread(this::mainLoopThread);
+        mainLoopTh = new Thread(mainLoop::mainLoopThread);
         acceptorTh.start();
         mainLoopTh.start();
 
@@ -72,5 +169,18 @@ public class Server {
         Thread thread = new Thread(this::run);
         thread.start();
         return thread;
+    }
+
+    synchronized public void shutdown() {
+        while (true) {
+            try {
+                eventQueue.put(new ShutdownEvent());
+                return;
+            } catch (InterruptedException ignored) {}
+        }
+    }
+
+    synchronized public ClientMessageStream localConnect() {
+        throw new RuntimeException("Not implemented yet!");
     }
 }
